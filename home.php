075f96bbeaceb539c660ca940d3b4d5c8f0394cd
arrawee <604234030@parichat.skru.ac.th>
2020-03-01 01:58:26 +0700

    3

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php require 'header.php'; ?>
+<div class="container">
+<div class="card mt-5">
+<div class="card-header">
+   <center><h2>หน้าหลักโปรแกรม</h2></center>
+   <div >
+        <a href="create.php"><input type="button"value="เพิ่มรายการ รายรับ-รายจ่าย"></a> 
+   </div>
+   <?php require 'footer.php';?>
