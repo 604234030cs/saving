df798b9c21443152a1e38f36f9c6172c871a4bed
arrawee <604234030@parichat.skru.ac.th>
2020-03-01 01:25:44 +0700

    2

--- create.php
+++ create.php
@@ -2,18 +2,17 @@
 require 'db.php';
 $message = '';
 if (isset ($_POST['submit'])) {
-    
     $user_id = $_POST['user_id'];
     $list_type_id = $_POST['list_type_id'];
     $list_name = $_POST['list_name'];
     $amount = $_POST['amount'];
     $date = $_POST['date'];
-    $sql = "INSERT INTO listsaving('$user_id','$list_type_id','$list_name','$amount','$date')";
+    $sql = 'INSERT INTO listsaving(user_id,list_type_id,list_name,amount,date)VALUES(?,?,?,?,?)';
     echo $sql;
     $statement  = $connection->prepare($sql);
     if ($statement->execute([$user_id,$list_type_id,$list_name,$amount,$date])){
         $message = 'data inserted successfully';
-        echo "The New Inserted Id Is:" .$conn ->insert_list_id;
+        
 
     }
     else
@@ -30,8 +29,8 @@ if (isset ($_POST['submit'])) {
 ?>
 <?php require 'header.php'; ?>
 <div class="container">
-    <div class="card mt-5">
-        <div class="card-header">
+<div class="card mt-5">
+<div class="card-header">
            <center><h2>เพิ่มรายการ รายรับ-รายจ่าย</h2></center> 
         </div>
         <div class="card-body">
@@ -43,20 +42,20 @@ if (isset ($_POST['submit'])) {
             <form method="post">
                 <div class="form-group">
                     <label for="user_id	">รหัสผู้ใช้</label>
-                    <input type="text" name="user_id"  class="form-control" require>
+                    <input type="text" name="user_id" id="user_id"  class="form-control" require>
                 </div>
                 <div class="form-group">
                     <label for="list_type_id">ประเภทรายการ</label><br>
-                    <input type="radio" name="list_type_id" value="1">  รายรับ
-                    <input type="radio" name="list_type_id" value="2">  รายจ่าย
+                    <input type="radio" name="list_type_id"id="list_type_id" value="1" class="frome-control">  รายรับ
+                    <input type="radio" name="list_type_id"id="list_type_id" value="2" class="frome-control">  รายจ่าย
                 </div>
                 <div class="form-group">
                     <label for="list_name">รายละเอียดรายการ</label>
-                    <input type="text" name="list_name"  class="form-control">
+                    <input type="text" name="list_name"id="list_name"  class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="amount">จำนวนเงิน</label>
-                    <input type="text" name="amount"  class="form-control">
+                    <input type="text" name="amount"id="amount"  class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="date">วันทีทำรายการ</label>
